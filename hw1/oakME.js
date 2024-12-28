ac0e709382dbcff4fa974a95b65e734d52c1a3b4
VinsOrl <109324683+VinsOrl@users.noreply.github.com>
2024-12-28 13:11:26 +0800

    Update oakME.js

--- hw1/oakME.js
+++ hw1/oakME.js
@@ -1,34 +1,24 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+const express = require("express");
+const app = express();
+const PORT = 8000;
 
-const app = new Application();
+app.use((req, res) => {
+  console.log("url =", req.url); // Log the request URL
+  let pathname = req.url;
 
-app.use((ctx) => {
-  // ctx.response.status = 404
-  console.log("url=", ctx.request.url);
-  let pathname = ctx.request.url.pathname;
-  if (pathname == "/") {
-    ctx.response.body = `<html>
-<body>
-<h1>我的自我介紹</h1>
-<ol>
-<li><a href="/name">姓名</a></li>
-<li><a href="/age">年齡</a></li>
-<li><a href="/gender">性別</a></li>
-</ol>
-</body>
-</html>
-`;
-  } else if (pathname == "/name") {
-    ctx.response.body = "林新興";
-  } else if (pathname == "/age") {
-    ctx.response.body = "19";
-  } else if (pathname == "/gender") {
-    ctx.response.body = "男";
+  if (pathname === "/name") {
+    res.send("林新興");
+  } else if (pathname === "/age") {
+    res.send("19");
+  } else if (pathname === "/gender") {
+    res.send("male");
   } else {
-    ctx.response.status = 404;
-    ctx.response.body = "Not Found!";
+    res.status(404).send("Not Found!");
   }
 });
 
-console.log("start at : http://127.0.0.1:8000");
-await app.listen({ port: 8000 });
+console.log("start at: http://127.0.0.1:8000");
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://127.0.0.1:${PORT}`);
+});
